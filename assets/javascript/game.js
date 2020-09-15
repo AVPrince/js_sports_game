d6648e8926e93eadf62f1a26fedf9020dcb5952a
avprince <shimmyaries@ALAN-TRACYs-MacBook-Air.local>
2020-09-15 12:33:15 -0500

    simplified java script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,24 +12,16 @@ teamOne.addEventListener('click', function(){
     console.log("Team One button clicked")
     let countValue = Number(shoot1.innerHTML) +1
     shoot1.innerHTML = countValue
+    let goalValue = Math.floor(Math.random()*2)+ Number(goal1.innerHTML)
+    goal1.innerHTML = goalValue
 })
 
 teamTwo.addEventListener('click', function(){
     console.log("Team Two button clicked")
     let countValue = Number(shoot2.innerHTML) +1
     shoot2.innerHTML = countValue
-})
-
-teamOne.addEventListener('click', function(){
-    // console.log("Team One Scores")
-    let countValue = Math.floor(Math.random()*2)+ Number(goal1.innerHTML)
-    goal1.innerHTML=countValue
-})
-
-teamTwo.addEventListener('click', function(){
-    // console.log("Team Two Scores")
-    let countValue = Math.floor(Math.random()*2) + Number(goal2.innerHTML)
-    goal2.innerHTML=countValue
+    let goalValue = Math.floor(Math.random()*2) + Number(goal2.innerHTML)
+    goal2.innerHTML=goalValue
 })
 
 resetButton.addEventListener('click', function(){
